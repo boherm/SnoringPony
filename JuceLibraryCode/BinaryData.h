dfc16c88946da7fcd070213954f17b38d6a28893
boherm <18699562+boherm@users.noreply.github.com>
2025-05-11 18:56:32 +0200

    Add jucer config

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,42 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   about_png;
+    const int            about_pngSize = 118622;
+
+    extern const char*   crash_png;
+    const int            crash_pngSize = 110194;
+
+    extern const char*   default_ponylayout;
+    const int            default_ponylayoutSize = 3506;
+
+    extern const char*   icon_png;
+    const int            icon_pngSize = 40809;
+
+    extern const char*   tray_icon_png;
+    const int            tray_icon_pngSize = 2548;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 5;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
