318c144db0dbbeb63c26e91e25f68389f689b2ac
boherm <18699562+boherm@users.noreply.github.com>
2026-05-20 22:58:49 +0200

    Bump to 1.0.0b6

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -54,7 +54,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "SnoringPony";
     const char* const  companyName    = "boherm";
-    const char* const  versionString  = "1.0.0b5";
+    const char* const  versionString  = "1.0.0b6";
     const int          versionNumber  = 0x10000;
 }
 #endif
